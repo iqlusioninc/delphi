b2f0cc7ae76b5796147b3eae5c07835ac9005a29
Tony Arcieri <tony@iqlusion.io>
2020-09-28 19:27:39 -0700

    Binance source
    API docs: https://github.com/binance-exchange/binance-official-api-docs/blob/master/rest-api.md
    
    This commit implements the `/api/v3/avgPrice` API.

--- src/sources.rs
+++ src/sources.rs
@@ -1,6 +1,7 @@
 //! Data sources
 
 pub mod alphavantage;
+pub mod binance;
 pub mod coinone;
 pub mod gdac;
 pub mod gopax;
@@ -116,11 +117,44 @@ impl PartialEq for PriceQuantity {
 /// Currencies for use in trading pairs
 #[derive(Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
 pub enum Currency {
+    /// Cosmos Atom
+    Atom,
+
+    /// Binance Coin
+    Bnb,
+
+    /// Binance KRW (won) stablecoin
+    Bkrw,
+
+    /// Bitcoin
+    Btc,
+
+    /// Binance USD stablecoin
+    Busd,
+
+    /// Ethereum
+    Eth,
+
+    /// Euro
+    Eur,
+
+    /// UK Pounds
+    Gbp,
+
     /// South Korean won
     Krw,
 
     /// Terra Luna
     Luna,
+
+    /// US dollars
+    Usd,
+
+    /// Circle stablecoin
+    Usdc,
+
+    /// Tether USDT stablecoin
+    Usdt,
 
     /// Other (open-ended)
     Other(String),
@@ -129,8 +163,19 @@ pub enum Currency {
 impl Display for Currency {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.write_str(match self {
+            Currency::Atom => "ATOM",
+            Currency::Bnb => "BNB",
+            Currency::Bkrw => "BKRW",
+            Currency::Btc => "BTC",
+            Currency::Busd => "BUSD",
+            Currency::Eth => "ETH",
+            Currency::Eur => "EUR",
+            Currency::Gbp => "GBP",
             Currency::Krw => "KRW",
             Currency::Luna => "LUNA",
+            Currency::Usd => "USD",
+            Currency::Usdc => "USDC",
+            Currency::Usdt => "USDT",
             Currency::Other(other) => other.as_ref(),
         })
     }
@@ -141,8 +186,19 @@ impl FromStr for Currency {
 
     fn from_str(s: &str) -> Result<Self, Error> {
         Ok(match s.to_ascii_uppercase().as_ref() {
+            "ATOM" => Currency::Atom,
+            "BNB" => Currency::Bnb,
+            "BKRW" => Currency::Bkrw,
+            "BTC" => Currency::Btc,
+            "BUSD" => Currency::Busd,
+            "ETH" => Currency::Eth,
+            "EUR" => Currency::Eur,
+            "GBP" => Currency::Gbp,
             "KRW" => Currency::Krw,
             "LUNA" => Currency::Luna,
+            "USD" => Currency::Usd,
+            "USDC" => Currency::Usdc,
+            "USDT" => Currency::Usdt,
             other => Currency::Other(other.to_owned()),
         })
     }
