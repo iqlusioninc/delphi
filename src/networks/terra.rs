c42fda50c6763882de49914d264e88b5aceff6c9
Shella Stephens <shella@iqlusion.io>
2020-10-03 11:07:58 -0700

    Update chain ID

--- src/networks/terra.rs
+++ src/networks/terra.rs
@@ -11,7 +11,7 @@ use once_cell::sync::Lazy;
 
 /// Chain ID
 // TODO(tarcieri): load from config
-pub const CHAIN_ID: &str = "columbus-3";
+pub const CHAIN_ID: &str = "columbus-4";
 
 /// Amount of gas to use when voting
 pub const GAS_AMOUNT: u64 = 200_000;
